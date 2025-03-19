85546d6d0848daf679b6bb33e113b449f5d1699f
Goder-0 <kai.na@cluml.com>
2025-03-19 14:23:29 +0900

    Solve Concatenation of Array
    use the repeat method

--- src/lib.rs
+++ src/lib.rs
@@ -12,4 +12,13 @@ mod tests {
         assert_eq!(8, task::add::sum(3, 5));
         assert_eq!(200, task::add::sum(100, 100));
     }
+
+    #[test]
+    fn concatenation() {
+        let v: Vec<i32> = vec![1, 2, 3, 4, 5];
+        assert_eq!(
+            vector::concatenation::concatenation_of_array(&v, 5),
+            vec![1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+        );
+    }
 }
---
+++ src/task/vector/concatenation.rs
@@ -0,0 +1,4 @@
+pub fn concatenation_of_array(nums: &Vec<i32>, n: usize) -> Vec<i32> {
+    let ans = nums.repeat(2);
+    ans
+}
