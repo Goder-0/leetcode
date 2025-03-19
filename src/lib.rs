65e2afec2742fc7fbd5ca9e1582d293844522d1b
Goder-0 <kai.na@cluml.com>
2025-03-19 13:13:48 +0900

    Solve add two integers
    use i16 because the sum of numbers from -100 to 100 fits within its range.

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,14 @@ pub mod task;
 
 #[cfg(test)]
 mod tests {
+
     use task::*;
 
     use super::*;
+
     #[test]
     fn add() {
-        let result = add::sum(2, 2);
-        assert_eq!(result, 4);
-    }
-
-    fn concatenation() {
-        let v: Vec<i32> = vec![1, 2, 3, 4, 5];
-        vector::concatenation::concatenation_of_array(v, 5);
+        assert_eq!(8, task::add::sum(3, 5));
+        assert_eq!(200, task::add::sum(100, 100));
     }
 }
---
+++ src/task/add.rs
@@ -0,0 +1,3 @@
+pub fn sum(num1: i16, num2: i16) -> i16 {
+    num1 + num2
+}
