e14951559c852892827168314b531be28b81256c
Goder-0 <kai.na@cluml.com>
2025-03-20 17:35:57 +0900

    Refactor code with stricter rules

--- src/task/vector/running_sum.rs
+++ src/task/vector/running_sum.rs
@@ -1,7 +1,8 @@
-pub fn running_sum_of_1d_array(nums: &Vec<i32>) -> Vec<i32> {
-    let mut ans:Vec<i32> = vec![nums[0]];
-    for i in 1..nums.len(){
-        ans.push(ans[i-1]+nums[i]);
+#[must_use]
+pub fn running_sum_of_1d_array(nums: &[i32]) -> Vec<i32> {
+    let mut ans: Vec<i32> = vec![nums[0]];
+    for i in 1..nums.len() {
+        ans.push(ans[i - 1] + nums[i]);
     }
     ans
 }
