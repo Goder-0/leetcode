af5752f340144a0aa470e5bd500bfe5e531ee632
Goder-0 <kai.na@cluml.com>
2025-03-25 10:08:52 +0900

    Refactor build array from permutation method

--- src/task/vector/permutation.rs
+++ src/task/vector/permutation.rs
@@ -1,9 +1,8 @@
 #[must_use]
-pub fn build_array_from_permutation(nums: &[i32]) -> Vec<i32> {
+pub fn build_array_from_permutation(nums: &[i32]) -> Vec<usize> {
     nums.iter()
-        .map(|&num| {
-            let index = usize::try_from(num).unwrap_or(0);
-            nums.get(index).copied().unwrap_or(0)
-        })
+        .filter_map(|&i| i.try_into().ok())
+        .filter_map(|index: usize| nums.get(index))
+        .filter_map(|&num| num.try_into().ok())
         .collect()
 }
