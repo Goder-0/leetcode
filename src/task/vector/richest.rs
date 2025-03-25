8dddeb52745db213e4f6ba79aea6a2e85c0731d0
Goder-0 <kai.na@cluml.com>
2025-03-25 15:17:27 +0900

    Refactor richest customer wealth method

--- src/task/vector/richest.rs
+++ src/task/vector/richest.rs
@@ -1,9 +1,8 @@
 #[must_use]
-pub fn richest_customer_wealth(accounts: &Vec<Vec<i32>>) -> i32 {
-    let mut ans = 0;
-    for account in accounts {
-        let sum = account.iter().sum();
-        ans = ans.max(sum);
-    }
-    ans
+pub fn richest_customer_wealth(accounts: &[Vec<i32>]) -> i32 {
+    accounts
+        .iter()
+        .map(|account| account.iter().sum())
+        .max()
+        .unwrap_or(0)
 }
