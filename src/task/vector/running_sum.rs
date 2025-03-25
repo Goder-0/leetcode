ab09f6d8abb30b35ef169680a3167c44e9306e18
Goder-0 <kai.na@cluml.com>
2025-03-25 13:28:33 +0900

    Refactor running sum of 1d array method

--- src/task/vector/running_sum.rs
+++ src/task/vector/running_sum.rs
@@ -1,8 +1,9 @@
 #[must_use]
 pub fn running_sum_of_1d_array(nums: &[i32]) -> Vec<i32> {
-    let mut ans: Vec<i32> = vec![nums[0]];
-    for i in 1..nums.len() {
-        ans.push(ans[i - 1] + nums[i]);
-    }
-    ans
+    nums.iter()
+        .scan(0, |s, &num| {
+            *s += num;
+            Some(*s)
+        })
+        .collect()
 }
