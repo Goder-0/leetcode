7be0a9b26fb1c43359cf750c5e6a3f4c4cdab77f
Goder-0 <kai.na@cluml.com>
2025-03-26 14:11:59 +0900

    Refactor subtract the product and sum of digits of an integer

--- src/task/numbers/substract.rs
+++ src/task/numbers/substract.rs
@@ -1,18 +1,11 @@
 #[must_use]
 pub fn subtract_the_product_and_sum_of_digits_of_an_integer(num: i32) -> u32 {
-    let numbers: Vec<u32> = num
+    let numbers = num
         .to_string()
         .chars()
         .map(|c| c.to_digit(10).unwrap_or(0))
-        .collect();
-
-    let mut sum = 0;
-    let mut product = 1;
-
-    for i in numbers {
-        sum += i;
-        product *= i;
-    }
-
-    product - sum
+        .fold((0, 1), |state: (u32, u32), number| {
+            (state.0 + number, state.1 * number)
+        });
+    numbers.1 - numbers.0
 }
