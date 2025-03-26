3eb9138503b8f4f10183b03ba45d365a1bd2f5df
Goder-0 <kai.na@cluml.com>
2025-03-26 14:34:53 +0900

    Refactor decode xored array

--- src/task/en_de_coding/xor.rs
+++ src/task/en_de_coding/xor.rs
@@ -1,9 +1,12 @@
 #[must_use]
 pub fn decode_xored_array(encoded: &[i32], first: i32) -> Vec<i32> {
-    let mut ans = vec![0; encoded.len() + 1];
-    ans[0] = first;
-    for i in 0..encoded.len() {
-        ans[i + 1] = ans[i] ^ encoded[i];
-    }
+    let mut ans: Vec<i32> = encoded
+        .iter()
+        .scan(first, |encode, &num| {
+            *encode ^= num;
+            Some(*encode)
+        })
+        .collect();
+    ans.insert(0, first);
     ans
 }
