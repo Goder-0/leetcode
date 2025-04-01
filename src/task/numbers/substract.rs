507a1ad40d8af09c4e0a051d7cd0cec0a68863a5
Goder-0 <kai.na@cluml.com>
2025-04-01 12:54:01 +0900

    Remove unwrap

--- src/task/numbers/substract.rs
+++ src/task/numbers/substract.rs
@@ -1,11 +1,10 @@
 #[must_use]
 pub fn subtract_the_product_and_sum_of_digits_of_an_integer(num: i32) -> u32 {
-    let numbers = num
+    let (sum, product) = num
         .to_string()
         .chars()
-        .map(|c| c.to_digit(10).unwrap_or(0))
-        .fold((0, 1), |state: (u32, u32), number| {
-            (state.0 + number, state.1 * number)
-        });
-    numbers.1 - numbers.0
+        .filter_map(|c| c.to_digit(10))
+        .fold((0, 1), |(s, p), n| (s + n, p * n));
+
+    product - sum
 }
