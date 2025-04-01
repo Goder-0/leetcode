c7f21aa21d3d1f1ac9d77e26b71d9b543aa4da0e
Goder-0 <kai.na@cluml.com>
2025-04-01 13:17:19 +0900

    Refactor Using fold

--- src/task/en_de_coding/convert.rs
+++ src/task/en_de_coding/convert.rs
@@ -1,12 +1,4 @@
 #[must_use]
-pub fn convert_binary_number_in_a_linked_list_to_integer(head: &[i32]) -> u32 {
-    let num = u32::from_str_radix(
-        &head
-            .iter()
-            .map(std::string::ToString::to_string)
-            .collect::<String>(),
-        2,
-    );
-
-    num.unwrap_or_default()
+pub fn convert_binary_number_in_a_linked_list_to_integer(head: &[i32]) -> i32 {
+    head.iter().fold(0, |num, &bit| (num << 1) + bit)
 }
