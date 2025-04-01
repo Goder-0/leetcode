afcbe2eb7a690c3ff24c7b6444fe00aee88e6287
Goder-0 <kai.na@cluml.com>
2025-04-01 13:36:03 +0900

    Add exception handling for zero input

--- src/task/iterations/reduce.rs
+++ src/task/iterations/reduce.rs
@@ -1,4 +1,4 @@
 #[must_use]
 pub fn number_of_steps_to_reduce_a_number_to_zero(num: u32) -> u32 {
-    31 + num.count_ones() - num.leading_zeros()
+    u32::BITS - num.leading_zeros() + num.count_ones() - u32::from(num != 0)
 }
